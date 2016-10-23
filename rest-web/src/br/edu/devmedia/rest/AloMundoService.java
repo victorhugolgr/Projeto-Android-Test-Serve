c54e7637e9da183def3611cc38c5644ed2697f16
Victor Hugo <victorhugolgr@gmail.com>
2016-10-23 15:59:47 -0300

    Aula: Lidando com parâmetros, produtores e consumidores - Curso de Android Studio - Aula 50

--- rest-web/src/br/edu/devmedia/rest/AloMundoService.java
+++ rest-web/src/br/edu/devmedia/rest/AloMundoService.java
@@ -1,13 +1,35 @@
 package br.edu.devmedia.rest;
 
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/")
 public class AloMundoService {
 
 	@GET
-	public String aloMundo() {
-		return "Alô Mundo!";
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String aloMundoGet(@QueryParam("msg") String msg) {
+		return "Alô Mundo GET! msg = " + msg;
+	}
+
+	@POST
+	public String aloMundoPost() {
+		return "Alô Mundo POST!";
+	}
+	
+	@GET
+	@Path("/param/{msg}")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String testeParam(@PathParam("msg") String msg) {
+		return "Param = " + msg;
 	}
 }
